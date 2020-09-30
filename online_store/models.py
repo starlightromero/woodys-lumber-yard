28bae1916236442f90940b9200bc68a269928c00
Starlight Romero <starlightromero@gmail.com>
2020-09-30 09:05:39 -0700

    added get_reset_token and verify_reset_token methods

--- online_store/models.py
+++ online_store/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import current_app
 from flask_login import UserMixin
 from passlib.hash import sha256_crypt
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -38,6 +39,21 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         """Check if given password matches hashed password."""
         return sha256_crypt.verify(password, self.password)
+
+    def get_reset_token(self, expires_sec=900):
+        """Generate user token to reset password."""
+        s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
+        return s.dumps({"user_id": self.id}).decode("utf-8")
+
+    @staticmethod
+    def verify_reset_token(token):
+        """Verify given token."""
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            user_id = s.loads(token)["user_id"]
+        except ValueError:
+            return None
+        return User.query.get(user_id)
 
 
 class Category(db.Model):
