dd3db648da95fe5b2ddf23f808e8e4dfdfc063f4
Starlight Romero <starlightromero@gmail.com>
2020-09-30 09:06:14 -0700

    initalized flask_mail

--- online_store/__init__.py
+++ online_store/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 from online_store.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+mail = Mail()
 login_manager.session_protection = "strong"
 login_manager.login_view = "users.login"
 
@@ -14,6 +16,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
 
     from online_store.main.routes import main
