f68110615c2a12fa0361d3d5f92d90bf695afbd0
Starlight Romero <starlightromero@gmail.com>
2020-10-03 14:14:52 -0700

    changed add-category to category

--- online_store/static/delete-category.js
+++ online_store/static/delete-category.js
@@ -6,10 +6,10 @@ const deleteCategory = categoryId => {
   axios.delete('/admin/category/' + categoryId)
     .then(function (response) {
       console.log(response)
-      window.location.href = '/admin/add-category'
+      window.location.href = '/admin/category'
     })
     .catch(function (error) {
       console.log(error)
-      window.location.href = '/admin/add-category'
+      window.location.href = '/admin/category'
     })
 }
