12b6828ad2dfee3cb89853179e5df9dfa2399737
Starlight Romero <starlightromero@gmail.com>
2020-10-03 21:09:28 -0700

    changed name to categories

--- online_store/static/categories.js
+++ online_store/static/categories.js
@@ -4,27 +4,27 @@
 
 const updateCategory = categoryId => {
   const categoryName = document.querySelector(`[data-id='${categoryId}']`)
-  axios.put('/admin/category/' + categoryId, {
+  axios.put('/admin/categories/' + categoryId, {
     name: categoryName.value
   })
     .then(function (response) {
       console.log(response)
-      window.location.href = '/admin/category'
+      window.location.href = '/admin/categories'
     })
     .catch(function (error) {
       console.log(error)
-      window.location.href = '/admin/category'
+      window.location.href = '/admin/categories'
     })
 }
 
 const deleteCategory = categoryId => {
-  axios.delete('/admin/category/' + categoryId)
+  axios.delete('/admin/categories/' + categoryId)
     .then(function (response) {
       console.log(response)
-      window.location.href = '/admin/category'
+      window.location.href = '/admin/categories'
     })
     .catch(function (error) {
       console.log(error)
-      window.location.href = '/admin/category'
+      window.location.href = '/admin/categories'
     })
 }
