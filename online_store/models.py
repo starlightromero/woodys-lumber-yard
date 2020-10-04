d768bddea809967fcaffc2bf2dd8e3ed158aa10d
Starlight Romero <starlightromero@gmail.com>
2020-10-04 16:17:26 -0700

    added Cart and ProductCartLink

--- online_store/models.py
+++ online_store/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
-from flask import current_app
+from flask import current_app, url_for
 from flask_login import UserMixin
+from flask_mail import Message
 from passlib.hash import sha256_crypt
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from online_store import db, login_manager
+from sqlalchemy.orm import backref
+from online_store import db, login_manager, mail
 
 
 @login_manager.user_loader
@@ -11,22 +13,25 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class BaseUser(db.Model, UserMixin):
-    """BaseUser database class."""
+class User(db.Model, UserMixin):
+    """User database class."""
 
-    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=True)
+    is_employee = db.Column(db.Boolean, nullable=False, default=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    categories = db.relationship("Category", backref="created_by", lazy=True)
+    products = db.relationship("Product", backref="created_by", lazy=True)
 
     def __repr__(self):
-        """Return username and email for BaseUser."""
+        """Return username and email for User."""
         return f"BaseUser('{self.username}', '{self.email}')"
 
     def __str__(self):
-        """Return username and email for BaseUser."""
+        """Return username and email for User."""
         return self.username
 
     def set_password(self, password):
@@ -42,6 +47,21 @@ class BaseUser(db.Model, UserMixin):
         s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
         return s.dumps({"user_id": self.id}).decode("utf-8")
 
+    def send_reset_email(self):
+        """Send password reset email."""
+        token = self.get_reset_token()
+        msg = Message(
+            "Password Reset Request",
+            recipients=[self.email],
+        )
+        msg.body = f"""To reset your password, visit the following link:
+
+    {url_for('users.reset_token', token=token, _external=True)}
+
+    If you did not make this request, please ignore this email.
+    """
+        mail.send(msg)
+
     @staticmethod
     def verify_reset_token(token):
         """Verify given token."""
@@ -53,29 +73,6 @@ class BaseUser(db.Model, UserMixin):
         return User.query.get(user_id)
 
 
-class User(BaseUser):
-    """User database class."""
-
-    is_employee = db.Column(db.Boolean, nullable=False, default=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
-
-
-class Employee(BaseUser):
-    """Employee database class."""
-
-    is_employee = db.Column(db.Boolean, nullable=False, default=True)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
-    categories = db.relationship("Category", backref="created_by", lazy=True)
-    products = db.relationship("Product", backref="created_by", lazy=True)
-
-
-class Admin(BaseUser):
-    """Admin database class."""
-
-    is_employee = db.Column(db.Boolean, nullable=False, default=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=True)
-
-
 class Category(db.Model):
     """Product Category database class."""
 
@@ -84,7 +81,7 @@ class Category(db.Model):
     link = db.Column(db.String(20), nullable=True)
     products = db.relationship("Product", backref="category", lazy=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    created_by_id = db.Column(db.Integer, db.ForeignKey("employee.id"))
+    created_by_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __repr__(self):
         """Return name for Category."""
@@ -97,6 +94,7 @@ class Category(db.Model):
     def add_link(self):
         """Add link based on category name."""
         self.link = self.name.lower().replace(" ", "-")
+        return self.link
 
 
 class Product(db.Model):
@@ -107,8 +105,10 @@ class Product(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
     price = db.Column(db.Float, nullable=False)
     image = db.Column(db.String(40), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    created_by_id = db.Column(db.Integer, db.ForeignKey("employee.id"))
+    created_by_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    carts = db.relationship("Cart", secondary="product_cart_link")
 
     def __repr__(self):
         """Return name and price for Product."""
@@ -117,3 +117,60 @@ class Product(db.Model):
     def __str__(self):
         """Return name and price for Product."""
         return f"Product('{self.name}', '{self.category}', '{self.price}')"
+
+    def set_product_quantity(self):
+        """Decrement product quantity."""
+        if self.quantity > 0:
+            self.quantity -= 1
+        return self.quantity
+
+
+class ProductCartLink(db.Model):
+    """ProductCartLink many-to-many database class."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
+    cart_id = db.Column(db.Integer, db.ForeignKey("cart.id"))
+    product = db.relationship(
+        "Product", backref=backref("link", cascade="all, delete-orphan")
+    )
+    cart = db.relationship(
+        "Cart", backref=backref("link", cascade="all, delete-orphan")
+    )
+
+
+class Cart(db.Model):
+    """Cart database class."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    products = db.relationship("Product", secondary="product_cart_link")
+    quantity = db.Column(db.Integer, default=0, nullable=False)
+    subtotal = db.Column(db.Float, default=0, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        """Return quantity and subtotal for Cart."""
+        return f"Cart('{self.quantity}', '${self.subtotal}')"
+
+    def __str__(self):
+        """Return quantity and subtotal for Cart."""
+        return f"Cart('{self.quantity}', '${self.subtotal}')"
+
+    def update_subtotal(self):
+        """Update cart subtotal."""
+        subtotal = 0
+        for product in self.products:
+            subtotal += product.price
+        self.subtotal = subtotal
+        return self.subtotal
+
+    def add_to_cart(self, product):
+        """
+        Increment product quantity in cart and append new prod to cart.
+        Call set product quantity on product.
+        """
+        self.products.append(product)
+        self.quantity += 1
+        product.set_product_quantity()
+        self.update_cart_subtotal()
+        return self
