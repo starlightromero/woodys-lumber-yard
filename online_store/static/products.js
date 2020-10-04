02d511612d0a7291973b3de75e3b62dd6fa7a177
Starlight Romero <starlightromero@gmail.com>
2020-10-03 22:33:51 -0700

    updated script

--- online_store/static/products.js
+++ online_store/static/products.js
@@ -3,13 +3,13 @@
 */
 
 const deleteProduct = productId => {
-  axios.delete('/admin/products' + productId)
+  axios.delete('/admin/products/' + productId)
     .then(function (response) {
       console.log(response)
-      window.location.href = '/admin'
+      window.location.href = '/admin/products'
     })
     .catch(function (error) {
       console.log(error)
-      window.location.href = '/admin'
+      window.location.href = '/admin/products'
     })
 }
