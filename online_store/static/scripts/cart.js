92e5a9c298874534826b8a7b3c5641e64c410066
Starlight Romero <starlightromero@gmail.com>
2020-10-05 00:19:42 -0700

    added clearCart function

--- online_store/static/scripts/cart.js
+++ online_store/static/scripts/cart.js
@@ -3,12 +3,36 @@
 */
 
 const removeFromCart = productId => {
-  console.log('delete')
   const product = document.getElementById(productId)
-  const quantity = product.querySelector('h3')
+  const price = product.querySelector('.price')
+  const quantityInCart = product.querySelector('.quantity-in-cart')
+  const totalProducts = document.getElementById('total-products')
+  const subtotal = document.getElementById('subtotal')
+  const quantity = +quantityInCart.innerHTML
   axios.delete('/cart/' + productId)
     .then(function (response) {
-      cartTotal.innerHTML = +cartTotal.innerHTML - +quantity.innerHTML
+      cartTotal.innerHTML = +cartTotal.innerHTML - quantity
+      product.remove()
+      totalProducts.innerHTML = +totalProducts.innerHTML - quantity
+      subtotal.innerHTML = +subtotal.innerHTML - ((quantity * +price.innerHTML).toFixed(2))
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+const clearCart = () => {
+  const allProducts = document.querySelectorAll('.cart-products')
+  const totalProducts = document.getElementById('total-products')
+  const subtotal = document.getElementById('subtotal')
+  axios.delete('/cart')
+    .then(function (response) {
+      cartTotal.innerHTML = 0
+      for (const product of allProducts) {
+        product.remove()
+      }
+      totalProducts.innerHTML = 0
+      subtotal.innerHTML = '0.00'
     })
     .catch(function (error) {
       console.log(error)
