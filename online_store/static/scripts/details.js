44d94932aece121c4e85c157bbc1a642bd90d61d
Starlight Romero <starlightromero@gmail.com>
2020-10-05 02:33:15 -0700

    created functions to check stock

--- online_store/static/scripts/details.js
+++ online_store/static/scripts/details.js
@@ -2,8 +2,26 @@
     axios
 */
 
+const formQuantity = document.querySelector('.form-quantity')
+const inStock = document.querySelector('.in-stock')
+const addToCartBtn = document.querySelector('.add-to-cart')
+
+const checkQuantity = () => {
+  const quantity = +formQuantity.value
+  if (quantity > +inStock.innerHTML || quantity < 1) {
+    addToCartBtn.disable = true
+    addToCartBtn.style.backgroundColor = '#B09C7D'
+  } else {
+    addToCartBtn.disable = false
+    addToCartBtn.style.backgroundColor = '#634C29'
+  }
+}
+
+formQuantity.addEventListener('keydown', checkQuantity)
+formQuantity.addEventListener('keyup', checkQuantity)
+formQuantity.addEventListener('change', checkQuantity)
+
 const addManyToCart = productId => {
-  const formQuantity = document.querySelector('.form-quantity')
   const quantity = +formQuantity.value
   axios.put('/cart/' + productId, {
     quantity: quantity
--- online_store/static/scripts/main.js
+++ online_store/static/scripts/main.js
@@ -10,11 +10,30 @@ searchBtn.addEventListener('click', () => {
 const cartTotal = document.querySelector('.cart-total')
 
 const addToCart = productId => {
+  const productItem = document.getElementById(productId)
   axios.put('/cart/' + productId)
     .then(function (response) {
       cartTotal.innerHTML = +cartTotal.innerHTML + 1
+      const newQuantity = +productItem.getAttribute('data-quantity') - 1
+      productItem.setAttribute('data-quantity', newQuantity)
+      checkQuantityInStock()
     })
     .catch(function (error) {
       console.log(error)
     })
 }
+
+const productItems = document.querySelectorAll('.product-item')
+
+const checkQuantityInStock = () => {
+  for (const prod of productItems) {
+    const addToCart = prod.querySelector('.add-to-cart')
+    if (+prod.getAttribute('data-quantity') === 0) {
+      addToCart.disabled = true
+      addToCart.style.backgroundColor = '#B09C7D'
+      addToCart.innerHTML = 'Sold Out'
+    }
+  }
+}
+
+checkQuantityInStock()
